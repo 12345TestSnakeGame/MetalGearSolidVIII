331209f4857415d0f2819cda8a2c2fd099840bec
giere <146406258@qq.com>
2020-03-24 10:10:21 +0800

    创建了几个基本的类，但是这种继承结果会不会过于繁琐？

--- LexicalAnalysis.py
+++ LexicalAnalysis.py
@@ -1,4 +1,85 @@
+import SyntaxAnalysis
+import pickle
 
+
+# 基本的finite automata
+# 正则文法的句法分析的表由SyntaxAnalysis产生，通过pickle文件读入
+# pickle文件的文件名作为默认参数
+class FA:
+    def __init__(self):
+        pass
+
+    # 根据文件名读取正则文法并compile:
+    #   · 使用正则句法的转换表进行句法分析
+    #   · 消除冗余
+    #   · 生成词法分析的转换表
+    def compile_regex(self, filename: str):
+        pass
+
+    # 输入一段text，生成词法分析的输出结果
+    def lexical_analyse(self, text):
+        pass
+
+    # 生成正则文法所产生的转换图
+    def diagram(self):
+        pass
+
+
+class DFA(FA):
+    def __init__(self):
+        super().__init__()
+
+    # 自己的补全
+    def compile_regex(self, filename: str):
+        pass
+
+    # 输入一段text，生成词法分析的输出结果
+    def lexical_analyse(self, text):
+        pass
+
+    # 生成正则文法所产生的转换图
+    def diagram(self):
+        pass
+
+
+class NFA(FA):
+    def __init__(self):
+        super().__init__()
+
+    # 自己的补全
+    def compile_regex(self, filename: str):
+        pass
+
+    # 输入一段text，生成词法分析的输出结果
+    def lexical_analyse(self, text):
+        pass
+
+    # 生成正则文法所产生的转换图
+    def diagram(self):
+        pass
+
+    def to_DFA(self):
+        pass
+
+
+class e_NFA(FA):
+    def __init__(self):
+        super().__init__()
+
+    # 自己的补全
+    def compile_regex(self, filename: str):
+        pass
+
+    # 输入一段text，生成词法分析的输出结果
+    def lexical_analyse(self, text):
+        pass
+
+    # 生成正则文法所产生的转换图
+    def diagram(self):
+        pass
+
+    def to_NFA(self):
+        pass
 
 
 if __name__ == '__main__':
