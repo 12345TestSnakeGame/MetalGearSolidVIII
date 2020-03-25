fcfaf5819374c1cb51c8a1eed415f88002f240a6
giere <146406258@qq.com>
2020-03-25 18:10:20 +0800

    初步实现了词法分析中结点，边以及构建短语的类设计

--- LexicalAnalysis.py
+++ LexicalAnalysis.py
@@ -1,5 +1,98 @@
-import SyntaxAnalysis
+from SyntaxAnalysis import *
 import pickle
+from graphviz import Digraph
+
+
+# 产生node，并分配序号
+# 可以生出一堆小node
+class FA_Node:
+    def __init__(self):
+        self.count = 0
+        self.nodes = {}
+
+    def generate(self):
+        new_node = _node(self.count)
+        self.nodes[self.count] = new_node
+        self.count += 1
+        return new_node
+
+    def __repr__(self):
+        return '=FA_Node with ' + str(self.count + 1) + ' nodes='
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __hash__(self):
+        return self.count
+
+
+# 只允许单字符，以及empty
+class Edge:
+    def __init__(self, symbol: str):
+        if len(symbol) != 1 and symbol != 'empty':
+            raise Exception('Edge:非法字符!')
+        else:
+            self.symbol = symbol
+
+    def __hash__(self):
+        return hash(self.symbol)
+
+    def __repr__(self):
+        return '--' + self.symbol + '->'
+
+    def __str__(self):
+        return self.__repr__()
+
+    def __eq__(self, other):
+        return self.symbol == other.symbol
+
+
+class _node:
+    def __init__(self, number: int):
+        self.number = number
+        self.next = {}
+
+    def add_node(self, symbol: Edge, other_node):
+        if symbol in self.next:
+            self.next[symbol].append(other_node)
+        else:
+            self.next[symbol] = [other_node]
+
+    def __repr__(self):
+        return '-node_' + str(self.number) + '-'
+
+    def __str(self):
+        return self.__repr__()
+
+    def __eq__(self, other):
+        return other.number == self.number
+
+    def __hash__(self):
+        return self.number
+
+
+class Phrase:
+    def __init__(self, Nodes: FA_Node, pre_phrase):
+        self.start = Nodes.generate()
+        self.end = Nodes.generate()
+        pre_phrase.end.add_node(Edge('empty'), self.start)
+
+    def concatenate(self, pre, post):
+        self.start.add_node(Edge('empty'), pre.start)
+        pre.end.add_node(Edge('empty'), post.start)
+        post.end.add_node(Edge('empty'), self.end)
+
+    def star(self, phrase):
+        self.start.add_node(Edge('empty'), self.end)
+        self.end.add_node(Edge('empty'), self.start)
+        self.start.add_node(Edge('empty'), phrase.start)
+        phrase.start.add_node(Edge('empty'), phrase.end)
+
+    def branch(self, p1, p2):
+        self.start.add_node(Edge('empty'), p1.start)
+        self.start.add_node(Edge('empty'), p2.start)
+        p1.end.add_node(Edge('empty'), self.end)
+        p2.end.add_node(Edge('empty'), self.end)
 
 
 # 基本的finite automata
@@ -68,6 +161,14 @@ class e_NFA(FA):
 
     # 自己的补全
     def compile_regex(self, filename: str):
+        pass
+
+    # 通过遍历整棵语法树的方法来生成自动机
+    def __traverse_compile(self):
+        pass
+
+    # TODO 通过设计SDT来在parsing时生成自动机
+    def __SDT_compile(self):
         pass
 
     # 输入一段text，生成词法分析的输出结果
